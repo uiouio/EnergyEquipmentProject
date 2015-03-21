31e4103c74cc2f4c082fce0064079173e8bffe81
Mashi <mashi596@hotmail.com>
2015-03-21 11:30:12 +0800

    分类显示入库单

--- KuGuanXiTong/Ruku_ShowStockDetail.Designer.cs
+++ KuGuanXiTong/Ruku_ShowStockDetail.Designer.cs
@@ -46,6 +46,10 @@
             this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.commonDataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -191,21 +195,62 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 21);
+            this.textBox1.TabIndex = 66;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "物品编码：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(257, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 68;
+            this.button3.Text = "查询";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(388, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 69;
+            this.button4.Text = "显示全部";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Ruku_ShowStockDetail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1092, 501);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.commonDataGridView1);
+            this.Controls.Add(this.label1);
             this.Name = "Ruku_ShowStockDetail";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "入库详情";
             this.Load += new System.EventHandler(this.Ruku_ShowStockDetail_Load);
             ((System.ComponentModel.ISupportInitialize)(this.commonDataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -225,5 +270,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- KuGuanXiTong/Ruku_ShowStockDetail.cs
+++ KuGuanXiTong/Ruku_ShowStockDetail.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using EntityClassLibrary;
@@ -11,12 +10,13 @@ using KuGuanXiTong.Service;
 using System.Collections;
 using DLLFullPrint;
 using CommonMethod;
-
+using System.Linq;
 
 namespace KuGuanXiTong
 {
     public partial class Ruku_ShowStockDetail : Form
     {
+        List<StockOperationDetail> detaillist = new List<StockOperationDetail>();
         OpStock opp = new OpStock();
         StockOperation sendStocktoshow = new StockOperation();
         /// <summary>
@@ -44,9 +44,12 @@ namespace KuGuanXiTong
             if (SendStocktoshow != null)
             {
                 IList details = opp.GetOpdetail(SendStocktoshow.Id);
+
                 int num = 1;
                 foreach(StockOperationDetail spd in details)
                 {
+                    detaillist.Add(spd);
+
                     this.commonDataGridView1.Rows.Add(num,spd.StockId.GoodsBaseInfoID.GoodsClassCode,
                         spd.StockId.GoodsBaseInfoID.GoodsName,
                         spd.GoodsCode,
@@ -58,11 +61,14 @@ namespace KuGuanXiTong
                         spd.StockId.StorehouseplaceCode,
                         spd.StockId.Quantity);
                     num++;
+               
                 }
-            
             }
         }
 
+
+
+       
         /// <summary>
         /// 打印按钮
         /// </summary>
@@ -70,28 +76,64 @@ namespace KuGuanXiTong
         /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
-            //DataTable dt = new DataTable();
-            //DataRow dr;
-            //设置列表头 
-
-            //foreach (DataGridViewColumn headerCell in commonDataGridView1.Columns)
-            //{
-            //    dt.Columns.Add(headerCell.HeaderText);
-            //}
-            //foreach (DataGridViewRow item in commonDataGridView1.Rows)
-            //{
-            //    dr = dt.NewRow();
-            //    for (int i = 0; i < dt.Columns.Count; i++)
-            //    {
-            //        dr[i] = item.Cells[i].Value.ToString();
-            //    }
-            //    dt.Rows.Add(dr);
-            //}
-            //DataSet dy = new DataSet();
-            //dy.Tables.Add(dt);
-            //MyDLL.TakeOver(dy);
             PrintDataGridView.PrintTheDataGridView(commonDataGridView1);
+        }
+
+        /// <summary>
+        /// 按编号查询
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.commonDataGridView1.Rows.Clear();
+            string likeed = this.textBox1.Text.Trim()+"%";
+            var resultList = from item in detaillist
+                             where item.StockId.GoodsBaseInfoID.GoodsClassCode.StartsWith(this.textBox1.Text.Trim())
+                             select item;
             
+            var escortList = resultList.ToList();
+            
+            int num = 1;
+            foreach (StockOperationDetail spd in escortList)
+            {
+                this.commonDataGridView1.Rows.Add(num, spd.StockId.GoodsBaseInfoID.GoodsClassCode,
+                    spd.StockId.GoodsBaseInfoID.GoodsName,
+                    spd.GoodsCode,
+                    spd.StockId.GoodsBaseInfoID.Specifications,
+                    spd.StockId.GoodsBaseInfoID.Material,
+                    spd.StockId.GoodsBaseInfoID.Unit,
+                    spd.Quantity,
+                    spd.TheMoney,
+                    spd.StockId.StorehouseplaceCode,
+                    spd.StockId.Quantity);
+                num++;
+            }
+        }
+
+        /// <summary>
+        /// 显示全部
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.commonDataGridView1.Rows.Clear();
+            int num = 1;
+            foreach (StockOperationDetail spd in detaillist)
+            {
+                this.commonDataGridView1.Rows.Add(num, spd.StockId.GoodsBaseInfoID.GoodsClassCode,
+                    spd.StockId.GoodsBaseInfoID.GoodsName,
+                    spd.GoodsCode,
+                    spd.StockId.GoodsBaseInfoID.Specifications,
+                    spd.StockId.GoodsBaseInfoID.Material,
+                    spd.StockId.GoodsBaseInfoID.Unit,
+                    spd.Quantity,
+                    spd.TheMoney,
+                    spd.StockId.StorehouseplaceCode,
+                    spd.StockId.Quantity);
+                num++;
+            }
         }
     }
 }
