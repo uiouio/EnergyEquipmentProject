6fe712485209b9cd57302810122def6db27ac7d5
liuyisai <1031348067@qq.com>
2015-10-21 21:28:45 +0800

    删除旧的office引用，

--- CommonMethod/ExportAndImport.cs
+++ CommonMethod/ExportAndImport.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Office.Core;
+//using Microsoft.Office.Core;
 using Microsoft.Office.Interop.Excel;
 using System.Data;
 
